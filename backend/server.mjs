2fe24b39c953f06250e8b940e0956a6dd21a9a34
AbdulRafay575 <136607014+AbdulRafay575@users.noreply.github.com>
2025-12-29 10:03:43 +0500

    Initial commit 1

--- backend/server.mjs
+++ backend/server.mjs
@@ -66,7 +66,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2300;
 // For NGINX proxy
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on 0.0.0.0:${PORT}`);
